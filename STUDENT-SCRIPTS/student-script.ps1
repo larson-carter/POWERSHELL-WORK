23fd0a1ff1b94d78f234f8a44456100e24bd23e9
Larson Carter <larsoncarter@blazermail.net>
2019-08-12 12:40:37 -0500

    Update student-script.ps1

--- STUDENT-SCRIPTS/student-script.ps1
+++ STUDENT-SCRIPTS/student-script.ps1
@@ -1,5 +1,3 @@
-try {
-
 # import AD module
 Import-Module ActiveDirectory
 
@@ -9,9 +7,6 @@ $file = "importCSV.csv"
 # import csv file
 $users = Import-Csv $file
 
-# Removes old log file, to show user new log...
-Remove-Item 'student-errors.txt'
-
 #FOR LOOP TO DETERMINE HOW MANY USERS
 ForEach($user in $users) {
 
@@ -19,69 +14,63 @@ ForEach($user in $users) {
 $name = $user.Name
 $givenname = $user.GivenName
 $surname = $user.Surname
-$ad_password = $user.Password
+$password = $user.Password
 $homedrive = $user.HomeDrive
 $displayname = $user.DisplayName
 $fullName = $user.SamAccountName
 $building = $user.building
 
-$ErrorActionPreference = "Continue"
-$OutputFileLocation = "student-errors.txt"
-Start-Transcript -path $OutputFileLocation -append
-
-$HomeDirectoryPartOne = "\\student\"
-If ($buiding -eq "3rd") {
-    $HomeDirectoryPartTwo = "3rdGrade$\"
+If ($buiding -eq "3") {
+    $HomeDirectoryPartTwo = "\\student\3rdGrade$\"
     $script = "3rdGrade.bat"
     $ou_path = "OU=3rdGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "4th") {
-    $HomeDirectoryPartTwo = "4thGrade$\"
+} ElseIf ($building -eq "4") {
+    $HomeDirectoryPartTwo = "\\student\4thGrade$\"
      $script = "4thGrade.bat"
      $ou_path = "OU=4thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "5th") {
-    $HomeDirectoryPartTwo = "5thGrade$\"
+} ElseIf ($building -eq "5") {
+    $HomeDirectoryPartTwo = "\\student\5thGrade$\"
      $script = "5thGrade.bat"
      $ou_path = "OU=5thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "6th") {
-    $HomeDirectoryPartTwo = "6thGrade$\"
+} ElseIf ($building -eq "6") {
+    $HomeDirectoryPartTwo = "\\student\6thGrade$\"
      $script = "6thGrade.bat"
      $ou_path = "OU=6thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "7th") {
-    $HomeDirectoryPartTwo = "7thGrade$\"
+} ElseIf ($building -eq "7") {
+    $HomeDirectoryPartTwo = "\\student\7thGrade$\"
      $script = "7thGrade.bat"
      $ou_path = "OU=7thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "8th") {
-    $HomeDirectoryPartTwo = "8thGrade$\"
+} ElseIf ($building -eq "8") {
+    $HomeDirectoryPartTwo = "\\student\8thGrade$\"
      $script = "8thGrade.bat"
      $ou_path = "OU=8thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "9th") {
-    $HomeDirectoryPartTwo = "9thGrade$\"
+} ElseIf ($building -eq "9") {
+    $HomeDirectoryPartTwo = "\\student\9thGrade$\"
      $script = "9thGrade.bat"
      $ou_path = "OU=9thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "10th") {
-    $HomeDirectoryPartTwo = "10thGrade$\"
+} ElseIf ($building -eq "10") {
+    $HomeDirectoryPartTwo = "\\student\10thGrade$\"
      $script = "10thGrade.bat"
      $ou_path = "OU=10thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "11th") {
-    $HomeDirectoryPartTwo = "11thGrade$\"
+} ElseIf ($building -eq "11") {
+    $HomeDirectoryPartTwo = "\\student\11thGrade$\"
      $script = "11thGrade.bat"
      $ou_path = "OU=11thGrade,OU=Students,DC=vv,DC=local"
-} ElseIf ($building -eq "12th") {
-    $HomeDirectoryPartTwo = "12thGrade$\"
+} ElseIf ($building -eq "12") {
+    $HomeDirectoryPartTwo = "\\student\12thGrade$\"
      $script = "12thGrade.bat"
      $ou_path = "OU=12thGrade,OU=Students,DC=vv,DC=local"
 } Else {
     "ERROR!!"
 }
 
-#ADDS ENTIRE HOME DIRECTORY
-$HomeDirectoryPutTogether = $HomeDirectoryPartOne + $HomeDirectoryPartTwo + $fullName
+$HomeDirectoryPutTogether = $HomeDirectoryPartTwo + $fullName
 
 #Create Home Folder
 New-Item -ItemType directory -Path $HomeDirectoryPutTogether
 
 # convert password to secure password
-$secure_password = ConvertTo-SecureString $ad_password -AsPlainText -Force
+$secure_password = ConvertTo-SecureString $password -AsPlainText -Force
 
 # create ad account
 New-ADUser -Name "$givenname$surname" -GivenName $givenname `
@@ -91,7 +80,7 @@ New-ADUser -Name "$givenname$surname" -GivenName $givenname `
 -HomeDirectory $HomeDirectoryPutTogether -HomeDrive $homedrive -ScriptPath $script
 
 #join to object group so it can map drives
-Add-ADGroupMember -Identity Students -Members $fullName
+Add-ADGroupMember -Identity Faculty -Members $fullName
 
 #DEFINE PERMISSIONS
 $path = "$HomeDirectoryPutTogether" #Replace with whatever file you want to do this to.
@@ -107,13 +96,5 @@ $perm = $user, $Rights, $InheritSettings, $PropogationSettings, $RuleType
 $rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $perm
 $acl.SetAccessRule($rule)
 $acl | Set-Acl -Path $path
-
-Stop-Transcript
-
-}
-
-} catch {
-
-    Write-Output "ERROR!: See Log File, student-errors.txt"
 
 }
